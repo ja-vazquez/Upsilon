8203403f6cb8d59f524f1bf37ea77cb3ad965c5d
Jose A <jvazquez@bnl.gov>
2016-04-15 14:25:06 -0400

    new data format for reading files

--- data/Useful_data.py
+++ data/Useful_data.py
@@ -1,43 +1,50 @@
 
 
+class Useful_data:
+    def __init__(self, name='Upsilon'):
+        self.name = name
 
-def file_choice(data_type):
-	#Select the type of file to analyze
-    if 'sim' in data_type:
-       bin_type = 'lin_rebin'                #lin or log / bin or rebin
-       redzz = ['0.25'] #,'0.40']
-       dir  = 'sim_results/'
+    def file_choice(self, data_type):
+        #"""Select the type of file to analyze"""
 
-    elif 'mocks' in data_type:
-       bin_type = 'rebin1'                    #lin1 or rebin1
-       redzz = ['singlesnap', 'allsnap', 'evol']
-       dir  = 'mock_results/'
+        if 'sim' in data_type:
+            bin_type = 'lin_rebin1'          # lin or log / bin or rebin
+            redz = ['0.25']                  # ,'0.40']
+            dir  = 'sim_results/'
 
-    elif 'lowz' in data_type:
-       bin_type = 'log1_rebin'              #log1 or log1_rebin
-       redzz = ['lowz'] #, 'z1', 'z2']
-       dir = 'lowz_results/'
+        elif 'mocks' in data_type:
+            bin_type = 'rebin1'              # lin1 or rebin1
+            redz = ['singlesnap']            # , 'allsnap', 'evol']
+            dir  = 'mock_results/'
 
-    return bin_type, redzz, dir
+        elif 'lowz' in data_type:
+            bin_type = 'log1_rebin'          # log1 or log1_rebin
+            redz = ['lowz']                  # , 'z1', 'z2']
+            dir = 'lowz_results/'
 
-
-
-def files_name(data_type, bin_type, redz):
-	#select the file's name
-     if 'sim' in data_type:
-        file_name = data_type +'_'+ bin_type +'_z'+ redz +'_norsd_np0.001_nRT10_r0'
-     elif 'mocks' in data_type:
-#        file_name = data_type +'_RST_'+ redz + bin_type + '_DM1_r0'
-	file_name = 'mock_bigMD_RST_' + redz + '_' + bin_type + '_DM1_r0'
-     elif 'lowz' in data_type:
-        file_name =  redz + '_' + bin_type + '_r0'
-     else:
-        print 'error'    
-    
-     return file_name
+        return bin_type, redz, dir
 
 
-def R0_files():
-    lnum = 2, 3, 4, 5, 6, 10
-    return lnum
 
+    def files_name(self, data_type, bin_type, redz):
+                #Select name of the file
+            if 'sim' in data_type:
+                file_name = data_type +'_'+ bin_type +'_z'+ redz +'_norsd_np0.001_nRT10_r0'
+            elif 'mocks' in data_type:
+                file_name = 'mock_bigMD_RST_' + redz + '_' + bin_type + '_DM1_r0'
+            elif 'lowz' in data_type:
+                file_name =  redz + '_' + bin_type + '_r0'
+            else:
+                print 'error'
+
+            return file_name
+
+
+    def R0_files(self):
+        lnum = 2, 3, 4, 5, 6, 10
+        return lnum
+
+
+if __name__ == '__main__':
+    Ud = Useful_data()
+    print Ud.file_choice('sim')
